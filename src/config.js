faf55cf9c5781804725424439d6aa8e9abaefae6
Wanda Antoine <wanda@thinkful.com>
2020-05-09 21:53:28 -0400

    added env

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 export default {
   API_ENDPOINT: `https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks`,
-  API_KEY: '$2a$10$ClPc/w4Q5OZPIR.qjitp2OVWcL7r/g6pihFo4yQX0kxHUy1OGbiEC',
+  // API_KEY: '$2a$10$ClPc/w4Q5OZPIR.qjitp2OVWcL7r/g6pihFo4yQX0kxHUy1OGbiEC',
+  API_KEY: process.env.REACT_APP_API_KEY,
 }
